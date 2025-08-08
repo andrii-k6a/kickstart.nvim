e5f7675041b8370bee2e4a7d75c9febab65e5c04
Andrii Kook <102363876+andrii-k6a@users.noreply.github.com>
2025-08-08 13:16:57 +0300

    oil keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -36,3 +36,6 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
+
+-- Oil keymaps
+vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open directory where the opened file is located' })
